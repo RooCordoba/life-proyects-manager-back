085c2ef873c42e2d594529bc47bc71504fe2660f
Roo <roco-fs@hotmail.com>
2024-07-23 17:30:05 -0300

    esqueleto del proyecto

---
+++ src/main/java/ar/myapp/proyectsmanager/ProyectsmanagerApplication.java
@@ -0,0 +1,13 @@
+package ar.myapp.proyectsmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectsmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectsmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/myapp/proyectsmanager/ProyectsmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.myapp.proyectsmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectsmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
