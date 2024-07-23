aa6819cac94f3143f2251d7b9855ab4c4e6ce2db
Roo <roco-fs@hotmail.com>
2024-07-23 17:47:29 -0300

    organizando un poco las carpetas y endpoint hello anda para probar

---
+++ src/main/java/ar/myapp/proyectsmanager/controllers/MainController.java
@@ -0,0 +1,15 @@
+package ar.myapp.proyectsmanager.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class MainController {
+
+    @GetMapping("hello")
+    public String getHello(){
+        return "Hello App";
+    }
+}
